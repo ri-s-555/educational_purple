c0658d8be0e7c26ef2aa9203967432fac6570aba
fullstackkk <alexsmit17264@gmail.com>
2025-02-05 13:29:35 +0700

    Добавил функцию генератор карточки товара

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,76 @@
 import '../css/style.css'
-console.log('hello')
+import {creatCard} from './card.js'
+
+ // прилетает с сервера
+const cards = [
+    {
+        color:'green',
+        price:0,
+        name:'',
+        review:0
+    },
+    {
+        color:'purpur',
+        price:0,
+        name:'',
+        review:0
+    },
+    {
+        color:'mint',
+        price:0,
+        name:'',
+        review:0
+    },
+    {
+        color:'green',
+        price:0,
+        name:'',
+        review:0
+    },
+    {
+        color:'purpur',
+        price:0,
+        name:'',
+        review:0
+    },
+    {
+        color:'mint',
+        price:0,
+        name:'',
+        review:0
+    },
+    {
+        color:'green',
+        price:0,
+        name:'',
+        review:0
+    },
+    {
+        color:'purpur',
+        price:0,
+        name:'',
+        review:0
+    },
+    {
+        color:'mint',
+        price:0,
+        name:'',
+        review:0
+    }
+
+]
+
+let cardsArray = []
+
+cards.forEach(item=>{
+    cardsArray.push( creatCard(item))
+})
+
+console.log(cardsArray)
+
+const cardsWrapper = document.querySelector('.sellers-product__cards-wrapper')
+console.log(cardsWrapper)
+
+cardsArray.forEach(item =>{
+    cardsWrapper.appendChild(item)
+})
